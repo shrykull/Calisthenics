9b7bd7bced9b988bcaece1d406aded2eb073b294
Yannick Kurtovic <y.kurtovic@tup.com>
2015-08-06 21:27:43 +0200

    More input

--- src/minesweeper/io/InputParser.java
+++ src/minesweeper/io/InputParser.java
@@ -1,12 +1,22 @@
 package minesweeper.io;
 
+import java.util.Scanner;
+
 import minesweeper.model.Position;
 
 public class InputParser {
- 
-	public Position parse(String input){
-	 int x = input.charAt(0);
-	 int y = input.charAt(1);
-	 return new Position(x, y);
- }
+
+	public Position parse(String input) {
+
+		String[] coords = input.split(":");
+		int x = Integer.parseInt(coords[0]);
+		int y = Integer.parseInt(coords[1]);
+		return new Position(x, y);
+	}
+	
+	public Position read(){
+		Scanner scan = new Scanner(System.in);
+		
+		return parse(scan.next());
+	}
 }
